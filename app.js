153628de13a8eed15c981a7b430127f5b63c4df0
Milton Lima <milton.mlima@gmail.com>
2022-09-04 19:00:23 -0300

    connecting nodejs to mysql and displaying in browser

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-var mysql = require('mysql');
+const { response } = require("express");
+const express = require("express");
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: ""
+const app = express();
+
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+var msg;
+con.connect(function (err) {
+    if (err) throw err;
+    msg = "Connected";
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+app.get("/", async (req, res) => {
+    if(!msg){
+        msg = "Not connected";
+    }
+    res.send(msg);
 });
+
+/*
+app.get("/primeira-rota", (request, response) => {
+    return response.json({
+        message: "Acessou a primeira rota com nodemon",
+    });
+});
+*/
+
+
+app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
