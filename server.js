84027807c81a2cc1a191e1d930340624e90b0e01
Milton Lima <milton.mlima@gmail.com>
2022-09-04 11:10:51 -0300

    creating routes

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const http = require("http");
 http
     .createServer((request, response) => {
         response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify({
-            message: 'Minha primeira aplicação com NodeJS'
-        }));
+
+        if (request.url === '/produto') {
+            response.end(JSON.stringify({
+                message: "Rota de produto",
+            })
+            );
+        } else if (request.url === '/usuario') {
+            response.end(JSON.stringify({
+                message: "Rota de usuário",
+            })
+            );
+        } else {
+            response.end(JSON.stringify({
+                mesage: "Outra rota",
+            })
+            );
+        }
+
+
 
     })
     .listen(4001, () => console.log("Servidor está rodando na porta 4001"));
